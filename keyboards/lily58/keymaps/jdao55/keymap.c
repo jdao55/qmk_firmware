c15a15a76bcc006f81398e4a949e8729c5058e86
Joseph Dao <joedao55@gmail.com>
2025-08-17 13:17:15 -0400

    Fix formating/typos

--- keyboards/lily58/keymaps/jdao55/keymap.c
+++ keyboards/lily58/keymaps/jdao55/keymap.c
@@ -14,7 +14,7 @@ enum {
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
+    // Tap once for Shift, twice for Caps Lock
     [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |BackSP|
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |   '  |
-     * |------+------+------+------+------+------|   [   |    |    ]* |------+------+------+------+------+------|
+     * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
      *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
@@ -61,31 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
      *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | del  | RGUI |
-     *                   |      |      |      |/     * /         \      \ |      |      |      |
+     *                   |      |      |      |/       /         \      \ |      |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
 
     [_RAISE] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_PLUS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______, _______, _______, _______, _______, KC_DEL, _______),
-    /* ADJUST
-     * ,-----------------------------------------.
-     * ,-----------------------------------------. |      |      |      |      |
-     * |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|
-     * |------+------+------+------+------+------| |      |      |      |      |
-     * |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|
-     * |------+------+------+------+------+------| |      |      |      |      |
-     * |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
-     * |------+------+------+------+------+------|       |    |
-     * |------+------+------+------+------+------| |      |      |      |      |
-     * |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
-     * `-----------------------------------------/       /
-     * \      \-----------------------------------------' | LAlt | LGUI |LOWER |
-     * /Space  /       \Enter \  |RAISE |BackSP| RGUI | |      |      |      |/
-     * /         \      \ |      |      |      |
-     *                   `----------------------------'
-     * '------''--------------------'
-     */
     [_ADJUST] = LAYOUT(XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______)};
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -112,6 +92,7 @@ const char *read_keylogs(void);
 // void set_timelog(void);
 // const char *read_timelog(void);
 
+//Render sailor moon logo
 static void render_logo(void) {
     static const char PROGMEM raw_logo[] = {
         0,   0,   0,   0,   0,   128, 192, 224, 112, 16,  8,   0, 0, 0, 0, 0,   0,   0,   0,   0,   0,   0,   0,   8,   16,  112, 224, 192, 128, 0,   0,   0, 0,  0,   0,   0,   0,   0,   0,  0, 0, 0, 0, 144, 176, 184, 176, 240, 176, 184, 176, 144, 0,   128, 64, 32, 0,  248, 0,  32, 64, 128, 0,  32, 32, 160, 96, 56,  32,  224, 32,  32,  0,   232, 112, 232, 112, 232, 32, 248, 32, 168, 0,  64, 64, 64, 64, 248, 64, 64,  64,  64,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0, 0, 0, 0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,   0,   248, 254, 255, 255, 243, 128, 0,   0,   0,   0,   0,  0, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   128, 243, 255, 255, 254, 248, 0,  0,  y0,  0,   0,   192, 192, 192, 192, 192, 0,   0,
