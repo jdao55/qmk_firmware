dbc3d0fe3763659ca1acbba5e018809d986ea348
joe dao <joedao55@gmail.com>
2025-09-18 19:14:04 -0400

    Remove keylog from oled display

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_NUM (1 << 1)
+#define L_SYM (1 << 2)
+#define L_NAV (1 << 3)
+#define L_GAME (1 << 4)
+#define L_ADJUST_TRI (L_NUM | L_SYM| L_NAV)
 
 char layer_state_str[24];
 
@@ -15,15 +16,18 @@ const char *read_layer_state(void) {
   case L_BASE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case L_NUM:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Number");
     break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+  case L_SYM:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Symbol");
     break;
-  case L_ADJUST:
+  case L_NAV:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Nav");
+    break;
+  case L_GAME:
   case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Game");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
